706270218786dec1b1cdaf8a09a74efdea571e85
luidimso <luidi_mso@outlook.com>
2021-05-15 17:08:22 -0300

    Put endpoint to update vehicle

--- models/vehicle.model.js
+++ models/vehicle.model.js
@@ -1,26 +1,14 @@
 import Joi from 'joi';
 import { validateRequest } from '../common/schema-validator.js';
 
-export function createVehicleSchema(req, res, next) {
+export function vehicleSchema(req, res, next) {
     const schema = Joi.object({
-        placa: Joi.string().required(),
+        placa: Joi.string().length(7).required(),
         chassi: Joi.string().required(),
         renavam: Joi.string().required(),
         modelo: Joi.string().required(),
         marca: Joi.string().required(),
         ano: Joi.number().required()
-    });
-    validateRequest(req, next, schema);
-}
-
-export function updateVehicleSchema(req, res, next) {
-    const schema = Joi.object({
-        placa: Joi.string().empty(''),
-        chassi: Joi.string().empty(''),
-        renavam: Joi.string().empty(''),
-        modelo: Joi.string().empty(''),
-        marca: Joi.string().empty(''),
-        ano: Joi.number().empty('')
     });
     validateRequest(req, next, schema);
 }
--- routes/vehicle.route.js
+++ routes/vehicle.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import vehicles from "../database/vehicles.json";
 import id from "../database/id.json";
 import fs from 'fs';
-import { createVehicleSchema, updateVehicleSchema } from '../models/vehicle.model.js';
+import { vehicleSchema } from '../models/vehicle.model.js';
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.get("/", (req, resp) => {
     resp.json(vehicles);
 });
 
-router.post("/", createVehicleSchema, (req, resp) => {
+router.post("/", vehicleSchema, (req, resp) => {
     var newVehicle = req.body;
     newVehicle["id"] = id.currentID;
     vehicles.push(req.body);
@@ -25,9 +25,30 @@ router.post("/", createVehicleSchema, (req, resp) => {
     }
 });
 
-router.put("/", updateVehicleSchema, (req, resp) => {
-    console.log(req.body);
-    resp.end();
+router.put("/:id", vehicleSchema, (req, resp) => {
+    var indexToUpdate = null;
+    var idToUpdate = null;
+
+    for(var i=0; i<vehicles.length; i++) {
+        if(req.params.id == vehicles[i].id) {
+            indexToUpdate = i;
+            idToUpdate = vehicles[i].id;
+        }
+    }
+
+    if(indexToUpdate != null) {
+        vehicles[idToUpdate] = req.body;
+        vehicles[idToUpdate]["id"] = idToUpdate;
+
+        try {
+            fs.writeFileSync("./database/vehicles.json", JSON.stringify(vehicles));
+            resp.status(200).send(vehicles);
+        } catch(err) {
+            resp.status(500).send(err);
+        }
+    } else {
+        resp.status(404).send({message: "Vehicle not found"});
+    }
 });
 
 router.delete("/", (req, resp) => {
