50dfa844cda72e662ef24e91934a0abc53cef5d4
luidimso <luidi_mso@outlook.com>
2021-05-15 16:15:43 -0300

    Post endpoint to create vehicle

--- routes/vehicle.route.js
+++ routes/vehicle.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import vehicles from "../database/vehicles.json";
+import id from "../database/id.json";
 import fs from 'fs';
 import { createVehicleSchema, updateVehicleSchema } from '../models/vehicle.model.js';
 
@@ -10,9 +11,18 @@ router.get("/", (req, resp) => {
 });
 
 router.post("/", createVehicleSchema, (req, resp) => {
+    var newVehicle = req.body;
+    newVehicle["id"] = id.currentID;
     vehicles.push(req.body);
-    fs.writeFileSync("./data/books.json", JSON.stringify(vehicles));
-    resp.status(200).send("OK");
+
+    try {
+        fs.writeFileSync("./database/vehicles.json", JSON.stringify(vehicles));
+        id.currentID++;
+        fs.writeFileSync("./database/id.json", JSON.stringify({currentID: id.currentID}));
+        resp.status(200).send("OK");
+    } catch(err) {
+        resp.status(500).send(err);
+    }
 });
 
 router.put("/", updateVehicleSchema, (req, resp) => {
